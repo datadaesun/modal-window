067fa8b33db0e00bec5ea5f490724f0ac5b98623
Daesun Pak <daesunsamuel0912@gmail.com>
2023-03-04 01:00:37 -0500

    Wrote the logic for opening and closing the modal.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-'use strict';
+"use strict";
+
+// variables
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
+const showModalsBtns = document.querySelectorAll(".show-modal");
+
+// functions
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModalsBtns.length; i++)
+  showModalsBtns[i].addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
